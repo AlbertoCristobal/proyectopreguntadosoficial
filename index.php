d102a4a7d263a20b326ffb830dd9b966073063fe
alberto <alberto@192.168.1.41>
2019-11-04 20:37:58 +0100

    contador de vidas y coockies

--- index.php
+++ index.php
@@ -13,13 +13,13 @@
            
             #imgLogo{
                 padding-top: 50px;
-                padding-left:37%;
+                padding-left:450px;
                 height:200px ;
             }
            
         </style>
     </head>
-    <body background="fondo1.jpg"  >
+    <body background="imagenes/fondo1.jpg"  >
         <?php
             include('./funciones.php');
             $mysqli = conectaBBDD();
@@ -52,9 +52,27 @@
 //            foreach ($numeros as $numero) {
 //                echo "$numero ";
 //            }
+            ?>
+        <!----------------------------------------------------------------------------------------------------------------->
+        <?php
+            if(isset($_COOKIE['contado'])){
+                setcookie('contado',$_COOKIE['contador']+1, time()+365*24*60*60);
+                echo "Numero de visitas:".$_COOKIE['contador'];
+            }else{
+                setcookie('contador',1, time()+365*24*60*60);
+                echo 'Bienvenido por primera vez a nuestra pagina';
+            }
+            
+            
 ?>
-        <img src="logo.jpg" id="imgLogo"></img>
-        <button name="tiempo" onclick="tiempoRestante()">Tiempo</button>
+        <img src="imagenes/logo.jpg" id="imgLogo"></img>
+        
+         <div id="vidas">
+            <img id="vida1" src="images/vida.png" width="30">
+            <img id="vida2" src="images/vida.png" width='30'>
+            <img id="vida3" src="images/vida.png" width='30'>
+        </div>
+        
         </br>
         <div class="container" id="preguntas">
             <div class="row">
@@ -97,10 +115,61 @@
         <script src="js/jquery-1.12.0.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
         <script>
+            /*
+             var puntuacion = 0;
+            
+            function chequea(opcion){
+                var correcta = '<?php echo $resultado;?>';
+                if(opcion == correcta){
+                    puntuacion = puntuacion + 100;
+                    document.getElementById("puntos").innerHTML = "<b>PUNTUACION: "+puntuacion+"</b>";
+                    $('#'+opcion).removeClass('btn btn-info').addClass('btn btn-success');
+                    document.title = "RESPUESTA CORRECTA!";
+                    document.getElementById("alert").innerHTML = "¡RESPUESTA CORRECTA!";
+                    $('#alert').removeClass('alert alert-warning').addClass('alert alert-success');
+                    setTimeout(
+                        function(){
+                            location.reload();
+                        },1000
+                    );                    
+                } else {
+                    $('#'+opcion).removeClass('btn btn-info').addClass('btn btn-danger');  
+                    document.title = "RESPUESTA ERRÓNEA!";
+                    document.getElementById("alert").innerHTML = "¡RESPUESTA ERRÓNEA!";
+                    $('#alert').removeClass('alert alert-warning').addClass('alert alert-danger');
+                    restaVidas();
+                }
+            }           
+            function restaVidas(){
+                var vidas = 3;
+                var imagen1 = $('#vida1').css('display');
+                var imagen2 = $('#vida2').css('display');
+                if(imagen1 == 'none' & imagen2 == 'none'){
+                    $('#vida3').css({'display':'none'});
+                    vidas = vidas-1;
+                }
+                else if(imagen1 == 'none'){
+                    $('#vida2').css({'display':'none'});
+                    vidas = vidas-1;
+                }
+                else if(imagen1 !== 'none'){
+                    $('#vida1').css({'display':'none'});
+                    vidas = vidas-1;
+                }
+                if(vidas == 0){
+                }
+            } 
+            */
+               
+                window.getCookie = function( name) {
+                var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+                if (match) return match[2];
+            };
+            
             
             var respuesta = '<?php echo $correcta?>';
              puntos=0;
-             tiempo=30;
+             
             
             function refresca(){
                 location.reload();
@@ -109,7 +178,8 @@
             function chequeaRespuesta(_respuesta){
                 if(respuesta ===_respuesta){
                    $('#Next').html('<button class="btn btn-success" onclick="refresca()">Next</buton>"');//cambia el boton de color al seleccionar la preguna correcta
-                   $('#btn1').html('Correcto');
+                   $('#btn1').addClass('Correcto');
+                   puntuacion=puntuacion + 10 ;
                     /*$('#Next').append('<button class="btn btn-success" onclick="refresca()">Next</buton>"');*/
                     
                 }
@@ -118,6 +188,9 @@
                    $('#btn2').html('Incorrecto');
                    $('#btn3').html('Incorrecto');
                    $('#btn4').html('Incorrecto');
+                   
+                   setCookie();
+                   document.getElementById('boton1').textContent = puntuacion;
                 }
                  
             }
@@ -127,21 +200,16 @@
                 window.history.back();
             }
              
-            function sumarPuntos(){
-               puntos++;
-               document.getElementById("puntos").innerHTML = "puntuacion:<b>"+ puntos + "</b>";
-               randNum = Math.round(Math.random()*500);
-               randNum2 = Math.round(Math.random()*500);
-              
-           }
             
-            function tiempoRestante(){
-                tiempo--;
-                document.getElementById(tiempo).innerHTML = "Tiempo:"+tiempo;
-                
+           
+              function setCookie(){
+                document.cookie = 'puntuacionC=' + puntuacion + '; expires=Fri, 19 Jun 2020 20:47:11 UTC; path=/';
             }
-            setInterval(restarTiempo,1000);
+               
+          
             
         </script>
+   
+        
     </body>
 </html>
